46152352192fdadf2ef8062689a142b21f8a68f3
Martin Kelly <mkelly@xevo.com>
2018-06-06 14:40:33 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+#
+# setuptools script for ws.
+#
+# Copyright (c) 2017-2018 Xevo Inc. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+import setuptools
+
+
+setuptools.setup(
+    name='ws-tool',
+    version='0.1',
+    description='A lightweight tool for managing a workspace of repositories',
+    author='Martin Kelly',
+    author_email='mkelly@xevo.com',
+    license='BSD',
+    python_requires='>=3',
+    install_requires=['PyYAML>=3.12'],
+    scripts=['bin/ws'],
+    classifiers=['Development Status :: 3 - Alpha',
+                 'Environment :: Console',
+                 'Intended Audience :: Developers',
+                 'License :: BSD',
+                 'Natural Language :: English',
+                 'Operating System :: POSIX :: Linux',
+                 'Programming Language :: Python :: 3 :: Only',
+                 'Topic :: Software Development :: Build Tools',
+                 ],
+)
